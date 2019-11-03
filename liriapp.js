5f8b14a3315a52b23324e0c1f5eaab62b45fa6bc
S Biswas <sbiswas1975@gmail.com>
2019-11-04 00:56:58 +0530

    command line usage

--- liriapp.js
+++ liriapp.js
@@ -1,6 +1,34 @@
-var http = require('http');
+/*var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello Worlds!');
-}).listen(8081);
+    res.end('Hello!');
+}).listen(8081);*/
+
+const prompt = require('prompt');
+
+const properties = [
+    {
+        name: 'username',
+        validator: /^[a-zA-Z\s\-]+$/,
+        warning: 'Username must be only letters, spaces, or dashes'
+    },
+    {
+        name: 'password',
+        hidden: true
+    }
+];
+
+prompt.start();
+
+prompt.get(properties, function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Command-line input received:');
+    console.log('  Username: ' + result.username);
+    console.log('  Password: ' + result.password);
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
